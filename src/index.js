1003534cb061df6c248bd6d74860fae914160b43
Romi58 <romaisanaz67@gmail.com>
2024-05-11 13:30:52 +0500

    Your commit message

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var countdown_1 = require("./countdown");
+var targetDate = new Date('2024-12-31T23:59:59');
+(0, countdown_1.default)(targetDate);
